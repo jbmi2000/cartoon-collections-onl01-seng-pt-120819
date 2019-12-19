79100875d3a91acf1d2cba25dc78cb5aafc4d346
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 16:55:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,8 +32,7 @@ end
 snacks = ["crackers","gouda","thyme"]
 cheese_types = ["cheddar", "gouda", "camembert"]
 def find_the_cheese(snacks, cheese_types)# code an argument here
-  snacks.find do |cheese|
-    cheese_types.include?(cheese)
+  snacks.detect{|cheese| cheese_types.include?(cheese)}
   end
   #if food the array below is here to help
 
